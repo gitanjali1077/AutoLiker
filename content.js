c6ecadd2356bc250da3e59b6c117fb8fda04bd24
gitanjali1077 <gitanjali1077@gmail.com>
2017-10-29 23:42:45 +0530

    FirstExtension Project

---
+++ content.js
@@ -0,0 +1,61 @@
+
+
+var a=document.getElementsByClassName("UFILikeLink _4x9- _4x9_ _48-k");
+ 
+/*
+css = 'h1 { background: red; }';
+style = document.createElement('style');
+
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+    
+  alert("hello" + a.length);
+for(i=0;i<a.length;i++)
+{
+ a[i].setAttribute("aria-pressed",true);
+ a[i].className+=" UFILinkBright";
+ a[i].appendChild(style);
+}
+  */
+var a1=document.getElementsByClassName("_eszkz _l9yih");
+
+var a11=document.getElementsByClassName("mUbCce fKz7Od GsLz7c teCjMb M9Bg4d");
+
+        var a12=document.getElementsByClassName("style-scope ytd-toggle-button-renderer");
+
+
+
+ var a22=document.getElementsByClassName("ProfileTweet-actionButton js-actionButton js-actionFavorite");
+
+for(i=0;i<a22.length;i++)
+{
+ a22[i].click();
+}
+
+for(i=0;i<a12.length;i++)
+{
+ a12[i].click();
+}
+/* for youtube*/
+
+for(i=0;i<a.length;i++)
+{
+ a[i].click();
+}
+ /* alert("facebook" + a.length);  */
+
+for(i=0;i<a1.length;i++)
+{
+ a1[i].click();
+}
+  /*  alert("twitter" + a1.length);    */
+for(i=0;i<a11.length;i++)
+{
+ a11[i].click();
+}
+   /* alert("google plus" + a11.length);   */ 
+
